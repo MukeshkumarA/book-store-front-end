3c67397cea2ef0961ba57c06239d57f036862e1c
Mukeshkumar Arumugam <101439718+MukeshkumarA@users.noreply.github.com>
2024-11-02 00:51:35 +0530

    chore: new backend service test

--- src/app/environment/environment-prod.ts
+++ src/app/environment/environment-prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     // apiUrl: 'https://book-store-0p9j.onrender.com'
     // apiUrl: 'https://book-store-0-0-1-2.onrender.com'
-    apiUrl: 'https://book-store-0-0-1.onrender.com'
+    // apiUrl: 'https://book-store-0-0-1.onrender.com'
+    apiUrl: 'https://book-store-latest-ppwl.onrender.com'
   };
   
